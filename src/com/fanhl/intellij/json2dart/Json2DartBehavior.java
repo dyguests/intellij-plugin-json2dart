233d79aac75290920ea0bbf3f9c8d3524028d9fc
fanhl <dyguests@qq.com>
2018-07-29 22:35:15 +0800

    MainAction

--- src/com/fanhl/intellij/json2dart/Json2DartBehavior.java
+++
@@ -1,9 +0,0 @@
-package com.fanhl.intellij.json2dart;
-
-/**
- * json 2 dart
- *
- * @author fanhl
- */
-public class Json2DartBehavior {
-}
---
+++ src/com/fanhl/intellij/json2dart/MainAction.java
@@ -0,0 +1,16 @@
+package com.fanhl.intellij.json2dart;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * json 2 dart
+ *
+ * @author fanhl
+ */
+public class MainAction extends AnAction {
+    @Override
+    public void actionPerformed(AnActionEvent anActionEvent) {
+
+    }
+}
