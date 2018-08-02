9612735915c009308d3d759f028bbe03c5fd8c2b
fanhl <fanhonglin@klicen.com>
2018-08-02 09:33:33 +0800

    generateClassEntity

--- src/com/fanhl/intellij/json2dart/processer/ConvertBridge.java
+++ src/com/fanhl/intellij/json2dart/processer/ConvertBridge.java
@@ -6,6 +6,9 @@ import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiComment;
 import com.intellij.psi.PsiElementFactory;
 import com.intellij.psi.PsiFile;
+import org.codehaus.jettison.json.JSONArray;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
 
 public class ConvertBridge {
 
@@ -24,23 +27,72 @@ public class ConvertBridge {
      * @param jsonStr      json字符串
      */
     public void convert(String classNameStr, String jsonStr) {
+        String commentStr = generateComment(classNameStr, jsonStr);
+
+        if (commentStr == null) {
+            return;
+        }
+
+        WriteCommandAction.runWriteCommandAction(project, () -> {
+            PsiElementFactory factory = PsiElementFactory.SERVICE.getInstance(project);
+            PsiComment comment = factory.createCommentFromText(commentStr, psiFile);
+            psiFile.addBefore(comment, psiFile.getFirstChild());
+        });
+    }
+
+    /**
+     * 这里要生成代码
+     * <p>
+     * **目前先生成comment
+     *
+     * @param classNameStr
+     * @param jsonStr
+     * @return
+     */
+    private String generateComment(String classNameStr, String jsonStr) {
+        if (jsonStr.startsWith("{")) {
+            JSONObject jsonObject;
+            try {
+                jsonObject = new JSONObject(jsonStr);
+            } catch (JSONException e) {
+                e.printStackTrace();
+                return null;
+            }
+
+            return generateClassEntity(classNameStr, jsonObject);
+        } else if (jsonStr.startsWith("[")) {
+            JSONArray jsonArray;
+            try {
+                jsonArray = new JSONArray(jsonStr);
+            } catch (JSONException e) {
+                e.printStackTrace();
+                return null;
+            }
+
+            return generateClassEntity(classNameStr, jsonArray);
+        } else {
+            return null;
+        }
+
+    }
+
+    private String generateClassEntity(String classNameStr, JSONObject jsonObject) {
+
         StringBuilder sb = new StringBuilder();
         sb.append("/*");
 
         sb.append("class ");
         sb.append(classNameStr);
         sb.append(" {\n");
-        sb.append(jsonStr);
+        sb.append(jsonObject.toString());
         sb.append("}\n");
 
         sb.append("*/\n");
 
-        WriteCommandAction.runWriteCommandAction(project, () -> {
-            PsiElementFactory factory = PsiElementFactory.SERVICE.getInstance(project);
-            PsiComment comment = factory.createCommentFromText(sb.toString(), psiFile);
-            psiFile.addBefore(comment, psiFile.getFirstChild());
-        });
+        return sb.toString();
+    }
 
-
+    private String generateClassEntity(String classNameStr, JSONArray jsonArray) {
+        return null;
     }
 }
