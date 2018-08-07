81f62941b209b3ce644dd9ce3e7425db7deda1d7
fanhl <dyguests@qq.com>
2018-08-07 22:52:12 +0800

    PsiDartFile

---
+++ src/com/fanhl/intellij/dart/psi/PsiDartFile.java
@@ -0,0 +1,13 @@
+package com.fanhl.intellij.dart.psi;
+
+import com.intellij.lang.Language;
+import com.intellij.lang.LanguageParserDefinitions;
+import com.intellij.psi.PsiClassOwner;
+import com.intellij.psi.PsiImportHolder;
+
+public interface PsiDartFile extends PsiImportHolder, PsiClassOwner {
+    static void a(){
+//        Language
+//        LanguageParserDefinitions.INSTANCE.forLanguage()
+    }
+}
